e510141f3524ffcc4b66e1ecaa93c13e4bdbd740
LeConsulat2 <wooine2@gmail.com>
2024-04-14 18:50:52 +1200

    ..

--- users/models.py
+++ users/models.py
@@ -4,5 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
+
+    profile_photo = models.ImageField()
     name = models.CharField(max_length=150, default="")
     is_host = models.BooleanField(default=False)
+
+
