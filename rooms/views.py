4b2ca0e4de19af7e7981a0d1cc11986584cf9a11
LeConsulat2 <wooine2@gmail.com>
2024-06-02 18:19:31 +1200

    11.25 Booking Completed!
    제발!!!

--- rooms/views.py
+++ rooms/views.py
@@ -230,6 +230,11 @@ class RoomBookings(APIView):
         room = self.get_object(pk)
         serializer = CreateRoomBookingSerializer(data=request.data)
         if serializer.is_valid():
-            return Response({"ok": True})
+            booking = serializer.save(
+                room=room,
+                user=request.user,
+                kind=Booking.BookingKindCHoices.ROOM,
+            )
+            return Response(serializer.data)
         else:
             return Response(serializer.errors)
