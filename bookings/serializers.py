990c796d7b0e4841a201f01eef876c1e5cfd32ca
LeConsulat2 <wooine2@gmail.com>
2024-06-02 18:16:07 +1200

    11.24 Validate Booking!

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -28,6 +28,21 @@ class CreateRoomBookingSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Cannot book in the past!")
         return value
 
+    def validate(self, data):
+        if data["check_out"] <= data["check_in]"]:
+            raise serializers.ValidationError(
+                "check in should be smaller than check out."
+            )
+
+        if Booking.objects.filter(
+            check_in__lte=data["cehck_out"],
+            check_out__gte=data["check_in"],
+        ).exists():
+            raise serializers.ValidationError(
+                "Those (or some) of those dates are already taken"
+            )
+        return data
+
 
 class PublicBookingSerializer(serializers.ModelField):
     class Meta:
